e5c91b063982582c61962642f7bc38d16ce677d2
Eno <eno_rami@ymail.com>
2024-05-27 09:14:27 +0200

    Homepage images est design

--- src/view/homepage.view.php
+++ src/view/homepage.view.php
@@ -2,13 +2,13 @@
 ob_start();
 ?>
 
-<div class="grid grid-cols-4 gap-4">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max-w-screen-xl py-8">
     <?php foreach ($hike_array as $hike) : ?>
-        <div class="card">
-            <img src="public/images/<?= htmlspecialchars($hike['id'] ?? 'default'); ?>.jpg" alt="<?= htmlspecialchars($hike['name']); ?>" class="w-full h-64 object-cover">
+        <div class="card bg-white shadow-xl rounded-lg overflow-hidden">
+            <img src="public/images/<?= htmlspecialchars($hike['id'] ?? 'default'); ?>.jpg" alt="<?= htmlspecialchars($hike['name']); ?>" class="w-full h-64 object-cover shadow-xl">
             <div class="p-4">
-                <h3 class="font-bold text-xl"><?= htmlspecialchars($hike["name"]); ?></h3>
-                <p>Distance: <?= htmlspecialchars($hike["distance"]); ?> KM</p>
+                <h3 class="font-bold text-xl pb-8 py-1"><?= htmlspecialchars($hike["name"]); ?></h3>
+                <p class="inline-block bg-blue-500 text-white px-2 py-0.5 rounded"><?= htmlspecialchars($hike["distance"]); ?>KM</p>
             </div>
         </div>
     <?php endforeach; ?>
