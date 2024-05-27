2770e77c0945f09df35a7aa92dd19c09fdc555e7
Eno <eno_rami@ymail.com>
2024-05-27 11:00:45 +0200

    Login in

--- src/view/user/login.view.php
+++ src/view/user/login.view.php
@@ -1,39 +1,50 @@
 <?php
 ob_start(); ?>
 
-<?php if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if ($error) { ?>
-        <!-- id countdownxxx usefull for Javascript redirection -->
-        <p><?php echo htmlspecialchars($error) ?></p>
-        <p>Redirection in <span id="countdownLogin">3</span> seconds</p>
-    <?php } else { ?>
-        <p><?php echo htmlspecialchars($success) ?></p>
-        <p>Redirection in <span id="countdownIndex">3</span> seconds</p>
-    <?php } ?>
-    <script>
-        sleepRedirect();
-    </script>
+<div class="relative min-h-screen flex items-center justify-center bg-gray-50 py-10 px-6 sm:px-6 lg:px-8 ">
+    <img src="public/images/forest.webp" alt="Background Image" class="absolute inset-0 h-full w-full object-cover z-0">
+    <div class="relative max-w-sm w-full space-y-8 z-10 p-10 bg-white bg-opacity-25 rounded-md border border-white" style="backdrop-filter: blur(10px);">
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-50">
+            Login in
+        </h2>
+        <?php if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            if ($error) { ?>
+                <p class="text-center text-red-500"><?php echo htmlspecialchars($error) ?></p>
+                <p class="text-center">Redirection in <span id="countdownLogin">3</span> seconds</p>
+            <?php } else { ?>
+                <p class="text-center text-green-500"><?php echo htmlspecialchars($success) ?></p>
+                <p class="text-center">Redirection in <span id="countdownIndex">3</span> seconds</p>
+            <?php } ?>
+            <script>
+                sleepRedirect();
+            </script>
+        <?php
+        } else { ?>
+            <form class="mt-8 space-y-6" method="post" action="<?php echo BASE_PATH; ?>/login/verification">
+                <input type="hidden" name="remember" value="true">
+                <div class="rounded-md shadow-sm -space-y-px">
+                    <div>
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+                    </div>
+                    <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-gray-50 bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+                        Sign in
+                    </button>
+                </div>
+            </form>
+        <?php } ?>
+    </div>
+</div>
+
 <?php
-} else { ?>
-    <form method="post" action="<?php echo BASE_PATH; ?>/login/verification">
-        <label for="email"></label>
-        <input type="text" id="email" name="email" placeholder="email" required>
-
-        <label for="password"></label>
-        <input type="password" id="password" name="password" placeholder="Password" required></br>
-
-        <input type="submit" class="btn" value="LOGIN">
-    </form>
-<?php } ?>
-
-
-
-<?php $contentBody = ob_get_clean();
+$contentBody = ob_get_clean();
 $contentHeader = "";
 $contentFooter = "";
-?>
-
-<?php
-//path from dirname, without, error
 require(__DIR__ . '/../layout.view.php');
-?>
