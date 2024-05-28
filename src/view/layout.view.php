3a4916a29c015e7a5bc67a6752bed441ff31f970
imadeusF <damien.fournier1981@gmail.com>
2024-05-28 09:18:30 +0200

    cor head

--- src/view/layout.view.php
+++ src/view/layout.view.php
@@ -1,34 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.16/dist/tailwind.min.css" rel="stylesheet">
-    <style>
-        .nav-link {
-            position: relative;
-            overflow: hidden;
-        }
-
-        .nav-link::after {
-            content: '';
-            position: absolute;
-            bottom: 0;
-            left: 0;
-            width: 0;
-            height: 1px;
-            background: white;
-            transition: width .3s;
-        }
-
-        .nav-link:hover::after {
-            width: 100%;
-        }
-    </style>
-    <title>Hikes</title>
-</head>
 
 <body>
     <?= $contentHeader ?>
