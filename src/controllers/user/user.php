7590424752120b552fd5cd712e552ab879da3615
Eno <eno_rami@ymail.com>
2024-05-30 15:15:59 +0200

    additional images for hikecreation

--- src/controllers/user/user.php
+++ src/controllers/user/user.php
@@ -5,12 +5,10 @@ namespace Application\Controllers\User;
 require_once('src/lib/database.php');
 require_once('src/model/user.php');
 require_once('src/model/hickescomments.php');
-require 'vendor/autoload.php';
 
 use Application\Lib\Database\DatabaseConnection;
 use Application\Model\User as UserModel;
 use Application\Model\Login as UserLogin;
-use Application\Model\HikesComments as HikesCommentsModel;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -86,38 +84,21 @@ class User
                         //Autologin after subscription
                         $id = $databaseConnection->getConnection()->lastInsertId();
 
-                        $_SESSION['user'] = [
-                            'sess_id' => $id,
-                            'sess_user' => $nickname
-                        ];
-
-                        $mail = new PHPMailer(true);
-
-                        try {
-                            //Server settings
-                            $mail->isSMTP();                                            //Send using SMTP
-                            $mail->Host       = 'live.smtp.mailtrap.io';                      //Set the SMTP server to send through
-                            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                            $mail->Username   = 'api';                               //SMTP username
-                            $mail->Password   = 'd9cd774fa76ca46bec6b3241cccab631'; //SMTP password
-                            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable explicit TLS encryption
-                            $mail->Port       = 587;                                    //TCP port to connect to
-
-                            //Recipients
-                            $mail->setFrom('live.smtp.mailtrap.io', 'Eno&Damien');
-                            $mail->addAddress($email);                                  //Add a recipient
-
-                            //Content
-                            $mail->isHTML(true);                                        //Set email format to HTML
-                            $mail->Subject = 'Welcome to our website!';
-                            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-                            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-                            $mail->send();
-                            echo 'Message has been sent';
-                        } catch (Exception $e) {
-                            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-                        }
+                        // Send email after successful registration
+                        $phpmailer = new PHPMailer();
+                        $phpmailer->isSMTP();
+                        $phpmailer->Host = 'smtp.enkelan.tech';
+                        $phpmailer->SMTPAuth = true;
+                        $phpmailer->Port = 587;
+                        $phpmailer->Username = 'api';
+                        $phpmailer->Password = 'YMYdG$R9';
+                        $phpmailer->setFrom('eno@enkelan.tech', 'Mailer');
+                        $phpmailer->addAddress($email, $nickname);     // Add a recipient
+                        $phpmailer->isHTML(true);                      // Set email format to HTML
+                        $phpmailer->Subject = 'Welcome to our website!';
+                        $phpmailer->Body    = 'This is the HTML message body <b>in bold!</b>';
+                        $phpmailer->AltBody = 'This is the body in plain text for non-HTML mail clients';
+                        $phpmailer->send();
                     }
                 }
             }
@@ -134,45 +115,32 @@ class User
 
         $user_infos = $newData->getUserInfos($userid);
 
-        //we check if the user connected is an admin, if yes, he will be able to edit and delete comments (see hikesdetails.view.php) condition || ($user_admin == "1")
-        $newData = new UserModel($databaseConnection);
-        $user_id = isset($_SESSION['user']['sess_id']) ? $_SESSION['user']['sess_id'] : null;
-        $user_admin = $newData->getUserAdminStatus($user_id);
-
         require(__DIR__ . '/../../view/user/showprofil.view.php');
     }
 
     public function SaveProfil($userid, $input, $env, $action)
     {
-        $databaseConnection = new DatabaseConnection($env);
-        $newData = new UserModel($databaseConnection);
-
-        if ($action == 'deleteprofil') {
-            $newData->DeleteUser($userid);
-
-            //we delete all comments of the user (model hikescomments.php)
-            $newData = new HikesCommentsModel($databaseConnection);
-            $newData->delAllCommentHicke($userid);
-
-            session_destroy();
-            echo "<script>window.location.href='" . BASE_PATH . "'</script>";
-            exit();
-        }
-
         $firstname = htmlspecialchars($input['firstname']);
         $lastname = htmlspecialchars($input['lastname']);
         $nickname = htmlspecialchars($input['nickname']);
         $email = filter_var($input['email'], FILTER_SANITIZE_EMAIL);
 
-        //we check if the user connected is an admin, if yes, he will be able to edit and delete comments (see hikesdetails.view.php) condition || ($user_admin == "1")
+        $databaseConnection = new DatabaseConnection($env);
         $newData = new UserModel($databaseConnection);
-        $user_id = isset($_SESSION['user']['sess_id']) ? $_SESSION['user']['sess_id'] : null;
-        $user_admin = $newData->getUserAdminStatus($user_id);
 
         $newData->SaveUserInfos($userid, $firstname, $lastname, $nickname, $email);
 
         if ($action == 'saveprofil') {
             $user_infos = $newData->getUserInfos($userid);
+        }
+
+        if ($action == 'deleteprofil') {
+            $newData->DeleteUser($userid);
+
+            session_start();
+            session_destroy();
+            header('Location: ' . BASE_PATH);
+            exit();
         }
         require(__DIR__ . '/../../view/user/showprofil.view.php');
     }
