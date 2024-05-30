a5c5655861183e6f6e51c3e63f806ca2eaa48d23
Eno <eno_rami@ymail.com>
2024-05-30 16:32:35 +0200

    creation du user new hike

--- src/view/homepage.view.php
+++ src/view/homepage.view.php
@@ -1,10 +1,29 @@
 <?php ob_start(); ?>
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mx-auto max-w-screen-xl py-8">
-    <?php foreach ($hike_array as $hike) : ?>
+    <?php
+    $counter = 0;
+    foreach ($hike_array as $hike) :
+        $counter++;
+    ?>
         <div class="card bg-white shadow-xl rounded-lg overflow-hidden flex flex-col">
             <div class="overflow-hidden">
                 <a href="<?php echo BASE_PATH; ?>/hikes/<?= htmlspecialchars($hike["id"]); ?>">
-                    <img src="<?php echo BASE_PATH; ?>/public/images/<?php echo htmlspecialchars($hike["id"] ?? 'default'); ?>.jpg" alt="<?= htmlspecialchars($hike['name']); ?>" class="w-full h-64 object-cover shadow-xl transform transition duration-500 hover:scale-110 cursor-pointer">
+                    <?php
+                    if ($counter <= 8) {
+                        // Use the existing logic for the first 8 hikes.
+                        $imagePath = BASE_PATH . "/public/images/" . htmlspecialchars($hike["id"] ?? 'default') . ".jpg";
+                    } else {
+                        // Get the category of the hike.
+                        $category = $hike["category"];
+
+                        // Generate a random number between 1 and 4.
+                        $randomNumber = rand(1, 4);
+
+                        // Construct the path to the random image.
+                        $imagePath = BASE_PATH . "/public/images/{$category}{$randomNumber}.jpg";
+                    }
+                    ?>
+                    <img src="<?= $imagePath ?>" alt="<?= htmlspecialchars($hike['name']); ?>" class="w-full h-64 object-cover shadow-xl transform transition duration-500 hover:scale-110 cursor-pointer">
                 </a>
             </div>
             <div class="p-4 flex flex-col justify-between h-1/2">
